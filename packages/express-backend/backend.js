587d38592b3b5fe22b392358d281eb34010c4fd1
clkodd <clkodden@calpoly.edu>
2024-01-29 17:35:54 -0800

    find user by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,7 +3,6 @@ import express from "express";
 
 const app = express();
 const port = 8000;
-
 const users = {
     users_list: [
       {
@@ -33,9 +32,21 @@ const users = {
       }
     ]
   };
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
 
   app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
   });
 
 app.use(express.json());
