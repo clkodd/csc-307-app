d57fadac191379b03137ba44b4119e38669c65a8
clkodd <clkodden@calpoly.edu>
2024-01-31 16:45:10 -0800

    delete user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,13 +36,24 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter((user) => user["name"] === name && user["job"] === job);
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
 };
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
+};
+const deleteUser = (userId) => {
+  let index = users["users_list"].findIndex((user) => user["id"] === userId);
+  if (index != -1) {
+    users["users_list"].splice(index, 1);
+    return index;
+  } else {
+    return index;
+  }
 };
 
 app.use(express.json());
@@ -58,13 +69,12 @@ app.get("/users", (req, res) => {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
-  } 
+  }
   if (name != undefined && job != undefined) {
     let result = findUserByNameAndJob(name, job);
     result = { users_list: result };
     res.send(result);
-  }
-  else {
+  } else {
     res.send(users);
   }
 });
@@ -79,24 +89,21 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
 });
 
-
-//
-//First, implement a hard delete operation to remove a particular user by id from the list. 
-// Hint: look at another HTTP method called DELETE. 
-//And remember this: always try to reuse existing URL patterns. 
-//Don't implement a route called '/delete-user'. 
-//Look at the resource you want to access and perform the delete action (you may have a URL pattern for it already).
-
-//Second, implement an additional action to get all users that match a given name and a given job. 
-//Hint: look at what we did in step 4 and extend it.
-
+app.delete("/users", (req, res) => {
+  const userIdToDelete = req.body.id; //or req.params.id
+  let result = deleteUser(userIdToDelete);
+  if (result != -1) {
+    res.send();
+  } else {
+    res.status(404).send("Resource not found.");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
